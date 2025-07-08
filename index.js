e547705936c8ca66fc3eeb3a1db912e1a4a1a0a9
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-07-08 19:13:28 +0600

    created server & users data send to server

---
+++ index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@meshal10613.mbbtx0s.mongodb.net/?retryWrites=true&w=majority&appName=meshal10613`;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async(req, res) => {
+    res.send("Server is running....");
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const usersCollection = client.db("shcolara").collection("users");
+
+        //usersCollection
+        app.get("/users", async(req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/users", async(req, res) => {
+            const { email } = req.body;
+            //chech user already exist or not
+            const existUser = await usersCollection.findOne({ email });
+            if(existUser){
+                const { lastSignInTime } = req.body;
+                const query = { email };
+                const updatedDoc = {
+                    $set: {
+                        lastSignInTime
+                    }
+                };
+                const result = await usersCollection.updateOne(query, updatedDoc);
+                return res.status(200).send(result);
+            };
+
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.status(201).send(result);
+        });
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
